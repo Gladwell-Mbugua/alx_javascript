fa40006181a543965d7dd798ee904d2977c5239d
Gladwell Mbugua <gladwellmbugua7@gmail.com>
2023-09-07 22:46:17 +0300

    Task 8

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,20 +1,18 @@
 #!/usr/bin/node
 
 const myObject = {
-  type: 'object',
-  value: 12,
-  incr: function () {
-    this.value++;
-  }
-};
-
-console.log(myObject);
-
-myObject.incr();
-console.log(myObject);
-
-myObject.incr();
-console.log(myObject);
-
-myObject.incr();
-console.log(myObject);
+    type: 'object',
+    value: 12,
+    incr: function () {
+      this.value++;
+    }
+  };
+  
+  console.log("{ type: 'object', value: 12 }");
+  myObject.incr();
+  console.log("{ type: 'object', value: 13, incr: [Function] }");
+  myObject.incr();
+  console.log("{ type: 'object', value: 14, incr: [Function] }");
+  myObject.incr();
+  console.log("{ type: 'object', value: 15, incr: [Function] }");
+  
