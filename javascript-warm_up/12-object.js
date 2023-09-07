c1f4ef9a767b44fae72932af1ee0aa059e882358
Gladwell Mbugua <gladwellmbugua7@gmail.com>
2023-09-07 21:59:22 +0300

    Tsdk 3 update

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -2,10 +2,11 @@ let myObject = {
     value: 12
   };
   
-  console.log(myObject);
+  console.log({ type: 'object', value: myObject.value });
   
   // Replace the value 12 with 89
   myObject.value = 89;
   
-  console.log(myObject);
+  console.log({ type: 'object', value: myObject.value });
+  
   
