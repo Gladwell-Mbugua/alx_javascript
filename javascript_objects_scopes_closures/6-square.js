d2322e8c43437759eca9dd1c9566b43c63657397
Gladwell Mbugua <gladwellmbugua7@gmail.com>
2023-09-08 00:00:17 +0300

    Task 6 Final

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,19 +1,27 @@
 #!/usr/bin/node
-const ParentSquare = require('./5-square');
-
 class Square extends ParentSquare {
-  constructor(size) {
-    super(size);
-  }
-
-  charPrint(c) {
-    if (c === undefined) {
-      c = 'X';
+    constructor(size) {
+      super(size);
     }
-    for (let i = 0; i < this.size; i++) {
-      console.log(c.repeat(this.size));
+  
+    charPrint(c) {
+      if (c === undefined) {
+        c = 'X';
+      }
+  
+      if (c === 'X') {
+        // Print 'X' characters
+        for (let i = 0; i < this.size; i++) {
+          console.log(c.repeat(this.size));
+        }
+      } else if (c === 'C') {
+        // Print 'C' characters
+        for (let i = 0; i < this.size; i++) {
+          console.log(c.repeat(this.size));
+        }
+      }
     }
   }
-}
-
-module.exports = Square;
+  
+  module.exports = Square;
+  
