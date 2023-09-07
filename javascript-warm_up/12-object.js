770bb1ed376410f42cda8a60eb26be3687bbcf17
Gladwell Mbugua <gladwellmbugua7@gmail.com>
2023-09-07 21:57:00 +0300

    update task 3

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,5 +1,11 @@
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 89, 13, 14, 15];
-
-numbers = numbers.map((number) => (number === 12 ? 89 : number));
-
-console.log(numbers);
+let myObject = {
+    value: 12
+  };
+  
+  console.log(myObject);
+  
+  // Replace the value 12 with 89
+  myObject.value = 89;
+  
+  console.log(myObject);
+  
